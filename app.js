30a31d029ed27c4b833672d13ec810fedc9f3ce0
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-05 15:28:09 +0900

    a태그를 사용한 서버 링크이동

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ const http = require('http');
 const server = http.createServer(function(request, response) {
   // console.log(request.url);
   if(request.url === "/") {
-    let doc = '<heml> <head> </head> <body> <h1> main </h1> </body> </html>'
+    let doc = '<heml> <head> </head> <body> <h1> <a href= "http://localhost:1234/sub">sub</a> </h1> </body> </html>'
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(doc)
   }
-  if(request.url === "/pingulee") {
-    let doc = '<heml> <head> </head> <body> <h1> pingulee </h1> </body> </html>'
+  if(request.url === "/sub") {
+    let doc = '<heml> <head> </head> <body> <h1> <a href= "http://localhost:1234">main</a> </h1> </body> </html>'
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(doc)
   }
-
-
-
-
-
 
 
   // console.log(request);
